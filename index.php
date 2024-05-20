ada8b722ab513b9f5176a52ae77b3a3aea2be982
dementriadelvin <148644890+StArViNn@users.noreply.github.com>
2024-05-20 09:32:12 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,28 +1,28 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <title>Pendaftaran Siswa Baru | SMK Coding</title>
+    <title>Pengolahan Buku | SMK Taruna Bhakti</title>
 </head>
 <link rel="stylesheet" href="style.css">
 
 <body>
     <header>
-        <h3>Pendaftaran Siswa Baru</h3>
+        <h3>Penambahan Buku</h3>
         <h1>SMK Taruna Bhakti</h1>
     </header>
 
     <h4>Menu</h4>
     <nav>
         <ul>
-            <li><a href="form-daftar.php">Daftar Baru</a></li>
-            <li><a href="list-siswa.php">Pendaftar</a></li>
+            <li><a href="form-daftar.php">Daftar Buku Baru</a></li>
+            <li><a href="list-siswa.php">Buku Terdaftar</a></li>
         </ul>
     </nav>
     <?php if(isset($_GET['status'])): ?>
     <p>
         <?php
             if($_GET['status'] == 'sukses'){
-                echo "Pendaftaran siswa baru berhasil!";
+                echo "Pendaftaran Buku baru berhasil!";
             } else {
                 echo "Pendaftaran gagal!";
             }
