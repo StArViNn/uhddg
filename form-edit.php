c1cba40bea7740fade4968aabd7a6dcc8d912dd7
dementriadelvin <148644890+StArViNn@users.noreply.github.com>
2024-05-20 09:31:41 +0700

    Update form-edit.php

--- form-edit.php
+++ form-edit.php
@@ -41,33 +41,16 @@ if( mysqli_num_rows($query) < 1 ){
             <input type="hidden" name="id" value="<?php echo $siswa['id'] ?>" />
 
         <p>
-            <label for="nama">Nama: </label>
-            <input type="text" name="nama" placeholder="nama lengkap" value="<?php echo $siswa['nama'] ?>" />
+            <label for="judulbuku">Judul Buku: </label>
+            <input type="text" name="judulbuku" placeholder="judulbuku" value="<?php echo $siswa['judulbuku'] ?>" />
         </p>
         <p>
-            <label for="alamat">Alamat: </label>
-            <textarea name="alamat"><?php echo $siswa['alamat'] ?></textarea>
+            <label for="pengarang">Pengarang: </label>
+            <textarea name="pengarang"><?php echo $siswa['pengarang'] ?></textarea>
         </p>
         <p>
-            <label for="jenis_kelamin">Jenis Kelamin: </label>
-            <?php $jk = $siswa['jenis_kelamin']; ?>
-            <label><input type="radio" name="jenis_kelamin" value="laki-laki" <?php echo ($jk == 'laki-laki') ? "checked": "" ?>> Laki-laki</label>
-            <label><input type="radio" name="jenis_kelamin" value="perempuan" <?php echo ($jk == 'perempuan') ? "checked": "" ?>> Perempuan</label>
-        </p>
-        <p>
-            <label for="agama">Agama: </label>
-            <?php $agama = $siswa['agama']; ?>
-            <select name="agama">
-                <option <?php echo ($agama == 'Islam') ? "selected": "" ?>>Islam</option>
-                <option <?php echo ($agama == 'Kristen') ? "selected": "" ?>>Kristen</option>
-                <option <?php echo ($agama == 'Hindu') ? "selected": "" ?>>Hindu</option>
-                <option <?php echo ($agama == 'Budha') ? "selected": "" ?>>Budha</option>
-                <option <?php echo ($agama == 'Atheis') ? "selected": "" ?>>Atheis</option>
-            </select>
-        </p>
-        <p>
-            <label for="sekolah_asal">Sekolah Asal: </label>
-            <input type="text" name="sekolah_asal" placeholder="nama sekolah" value="<?php echo $siswa['sekolah_asal'] ?>" />
+            <label for="tahun_terbit">Tahun Terbit: </label>
+            <input type="text" name="tahun_terbit" placeholder="nama sekolah" value="<?php echo $siswa['tahun_terbit'] ?>" />
         </p>
         <p>
             <input type="submit" value="Simpan" name="simpan" />
